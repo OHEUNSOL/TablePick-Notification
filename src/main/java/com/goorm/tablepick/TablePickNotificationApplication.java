fba8a78655cdd419b3b1dce1e3bcd765fe468c50
OHEUNSOL <eunsol037@naver.com>
2025-11-29 22:44:55 +0900

    refactor: 이름 변경

--- src/main/java/com/goorm/tablepick/TablePickNotificationApplication.java
+++ src/main/java/com/goorm/tablepick/TablePickNotificationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TablePickPaymentApplication {
+public class TablePickNotificationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TablePickPaymentApplication.class, args);
+        SpringApplication.run(TablePickNotificationApplication.class, args);
     }
 
 }
