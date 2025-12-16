8724dd6980148d4db75865d66e1384f1077dcb1d
OHEUNSOL <eunsol037@naver.com>
2025-12-16 17:40:08 +0900

    remove: dlt 로그 엔티티 삭제

--- src/main/java/com/goorm/tablepick/email/repository/DltEmailFailLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.goorm.tablepick.email.repository;
-
-import com.goorm.tablepick.email.entity.DltEmailFailLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DltEmailFailLogRepository extends JpaRepository<DltEmailFailLog, Long> {
-}
